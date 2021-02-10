dfe72adf38a3a49cba859aac92b7e6757370878e
ZdenekJ <zdenek.jasek@gmail.com>
2021-02-10 19:06:48 +0100

    Texty + okna

--- WatchDogs2/brb.js
+++ WatchDogs2/brb.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const windowDedsec = document.querySelector(".window--dedsec");
+const windowBlume = document.querySelector(".window--blume");
+const windowBrb = document.querySelector(".window--brb");
+
+const toggleWindow = (el, showClass = "window--show") =>
+  el.classList.toggle(showClass);
+
+setTimeout(() => {
+  toggleWindow(windowDedsec);
+}, 1000);
+
+setTimeout(() => {
+  toggleWindow(windowBrb);
+}, 2000);
+
+setTimeout(() => {
+  toggleWindow(windowBlume, "window--showQuickly");
+}, 3000);
